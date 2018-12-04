bdc5cc09aea2d2c3376840f58b7a954ceda6ad7c
lahaymd <lahaymd@gmail.com>
2018-12-04 13:00:49 -0600

    added debugger

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 console.log('created on github')
 // comment
 // comment from first branch
+
+
+if (true) {
+    let a = 2;
+    var b=4;
+    console.log(a);
+    console.log(b);
+    
+}
+
+// console.log(a);
+console.log(b);
